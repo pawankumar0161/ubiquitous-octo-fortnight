e8266549dfbf8b2aeb4c294dcf2ff3f3c96a176c
pawankumar0161 <pawankumar0161@outlook.com>
2022-02-26 23:37:04 +0530

    First Commit

---
+++ IRSSubmission.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IRSSubmission
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblPDFMsg = New System.Windows.Forms.Label()
+        Me.lblXMLMsg = New System.Windows.Forms.Label()
+        Me.lblManifestMsg = New System.Windows.Forms.Label()
+        Me.btnSendSubmission = New System.Windows.Forms.Button()
+        Me.btnPDF = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnUploadXML = New System.Windows.Forms.Button()
+        Me.lblXML = New System.Windows.Forms.Label()
+        Me.btnManifest = New System.Windows.Forms.Button()
+        Me.lblmanifest = New System.Windows.Forms.Label()
+        Me.txtSubmissionID = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.updManifest = New System.Windows.Forms.OpenFileDialog()
+        Me.updXML = New System.Windows.Forms.OpenFileDialog()
+        Me.updPDF = New System.Windows.Forms.OpenFileDialog()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btnGetAck = New System.Windows.Forms.Button()
+        Me.txtSubmissionIdAck = New System.Windows.Forms.TextBox()
+        Me.lblSubmissionIdAck = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.lblPDFMsg)
+        Me.Panel2.Controls.Add(Me.lblXMLMsg)
+        Me.Panel2.Controls.Add(Me.lblManifestMsg)
+        Me.Panel2.Controls.Add(Me.btnSendSubmission)
+        Me.Panel2.Controls.Add(Me.btnPDF)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.btnUploadXML)
+        Me.Panel2.Controls.Add(Me.lblXML)
+        Me.Panel2.Controls.Add(Me.btnManifest)
+        Me.Panel2.Controls.Add(Me.lblmanifest)
+        Me.Panel2.Controls.Add(Me.txtSubmissionID)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel2.Location = New System.Drawing.Point(28, 38)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(393, 424)
+        Me.Panel2.TabIndex = 3
+        '
+        'lblPDFMsg
+        '
+        Me.lblPDFMsg.AutoSize = True
+        Me.lblPDFMsg.Location = New System.Drawing.Point(140, 276)
+        Me.lblPDFMsg.Name = "lblPDFMsg"
+        Me.lblPDFMsg.Size = New System.Drawing.Size(0, 17)
+        Me.lblPDFMsg.TabIndex = 14
+        '
+        'lblXMLMsg
+        '
+        Me.lblXMLMsg.AutoSize = True
+        Me.lblXMLMsg.Location = New System.Drawing.Point(140, 218)
+        Me.lblXMLMsg.Name = "lblXMLMsg"
+        Me.lblXMLMsg.Size = New System.Drawing.Size(0, 17)
+        Me.lblXMLMsg.TabIndex = 13
+        '
+        'lblManifestMsg
+        '
+        Me.lblManifestMsg.AutoSize = True
+        Me.lblManifestMsg.Location = New System.Drawing.Point(140, 169)
+        Me.lblManifestMsg.Name = "lblManifestMsg"
+        Me.lblManifestMsg.Size = New System.Drawing.Size(0, 17)
+        Me.lblManifestMsg.TabIndex = 12
+        '
+        'btnSendSubmission
+        '
+        Me.btnSendSubmission.Location = New System.Drawing.Point(107, 304)
+        Me.btnSendSubmission.Name = "btnSendSubmission"
+        Me.btnSendSubmission.Size = New System.Drawing.Size(200, 47)
+        Me.btnSendSubmission.TabIndex = 11
+        Me.btnSendSubmission.Text = "Send Submission"
+        Me.btnSendSubmission.UseVisualStyleBackColor = True
+        '
+        'btnPDF
+        '
+        Me.btnPDF.Location = New System.Drawing.Point(143, 242)
+        Me.btnPDF.Name = "btnPDF"
+        Me.btnPDF.Size = New System.Drawing.Size(148, 24)
+        Me.btnPDF.TabIndex = 10
+        Me.btnPDF.Text = "Upload PDF file"
+        Me.btnPDF.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 243)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 17)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "PDF Attachment"
+        '
+        'btnUploadXML
+        '
+        Me.btnUploadXML.Location = New System.Drawing.Point(143, 191)
+        Me.btnUploadXML.Name = "btnUploadXML"
+        Me.btnUploadXML.Size = New System.Drawing.Size(148, 24)
+        Me.btnUploadXML.TabIndex = 8
+        Me.btnUploadXML.Text = "Upload XML file"
+        Me.btnUploadXML.UseVisualStyleBackColor = True
+        '
+        'lblXML
+        '
+        Me.lblXML.AutoSize = True
+        Me.lblXML.Location = New System.Drawing.Point(29, 192)
+        Me.lblXML.Name = "lblXML"
+        Me.lblXML.Size = New System.Drawing.Size(62, 17)
+        Me.lblXML.TabIndex = 7
+        Me.lblXML.Text = "XML File"
+        '
+        'btnManifest
+        '
+        Me.btnManifest.Location = New System.Drawing.Point(143, 142)
+        Me.btnManifest.Name = "btnManifest"
+        Me.btnManifest.Size = New System.Drawing.Size(148, 24)
+        Me.btnManifest.TabIndex = 6
+        Me.btnManifest.Text = "Upload Manifest file"
+        Me.btnManifest.UseVisualStyleBackColor = True
+        '
+        'lblmanifest
+        '
+        Me.lblmanifest.AutoSize = True
+        Me.lblmanifest.Location = New System.Drawing.Point(29, 143)
+        Me.lblmanifest.Name = "lblmanifest"
+        Me.lblmanifest.Size = New System.Drawing.Size(61, 17)
+        Me.lblmanifest.TabIndex = 5
+        Me.lblmanifest.Text = "Manifest"
+        '
+        'txtSubmissionID
+        '
+        Me.txtSubmissionID.Location = New System.Drawing.Point(143, 95)
+        Me.txtSubmissionID.Name = "txtSubmissionID"
+        Me.txtSubmissionID.Size = New System.Drawing.Size(164, 23)
+        Me.txtSubmissionID.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Submission ID"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(79, 35)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(228, 26)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Send Submission Section"
+        '
+        'updManifest
+        '
+        Me.updManifest.FileName = "UploadManifest"
+        '
+        'updXML
+        '
+        Me.updXML.FileName = "updXML"
+        '
+        'updPDF
+        '
+        Me.updPDF.FileName = "updPDF"
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.btnGetAck)
+        Me.Panel3.Controls.Add(Me.txtSubmissionIdAck)
+        Me.Panel3.Controls.Add(Me.lblSubmissionIdAck)
+        Me.Panel3.Controls.Add(Me.Label8)
+        Me.Panel3.Location = New System.Drawing.Point(442, 40)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(497, 422)
+        Me.Panel3.TabIndex = 4
+        '
+        'btnGetAck
+        '
+        Me.btnGetAck.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGetAck.Location = New System.Drawing.Point(199, 170)
+        Me.btnGetAck.Name = "btnGetAck"
+        Me.btnGetAck.Size = New System.Drawing.Size(164, 30)
+        Me.btnGetAck.TabIndex = 12
+        Me.btnGetAck.Text = "Get Acknowledgement"
+        Me.btnGetAck.UseVisualStyleBackColor = True
+        '
+        'txtSubmissionIdAck
+        '
+        Me.txtSubmissionIdAck.Location = New System.Drawing.Point(199, 127)
+        Me.txtSubmissionIdAck.Name = "txtSubmissionIdAck"
+        Me.txtSubmissionIdAck.Size = New System.Drawing.Size(164, 20)
+        Me.txtSubmissionIdAck.TabIndex = 6
+        '
+        'lblSubmissionIdAck
+        '
+        Me.lblSubmissionIdAck.AutoSize = True
+        Me.lblSubmissionIdAck.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubmissionIdAck.Location = New System.Drawing.Point(88, 130)
+        Me.lblSubmissionIdAck.Name = "lblSubmissionIdAck"
+        Me.lblSubmissionIdAck.Size = New System.Drawing.Size(97, 17)
+        Me.lblSubmissionIdAck.TabIndex = 5
+        Me.lblSubmissionIdAck.Text = "Submission ID"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(102, 36)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(272, 26)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "Get Acknowledgement Section"
+        '
+        'IRSSubmission
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(970, 500)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Name = "IRSSubmission"
+        Me.Text = "IRS Send Submission"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtSubmissionID As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblmanifest As Label
+    Friend WithEvents btnManifest As Button
+    Friend WithEvents updManifest As OpenFileDialog
+    Friend WithEvents btnUploadXML As Button
+    Friend WithEvents lblXML As Label
+    Friend WithEvents updXML As OpenFileDialog
+    Friend WithEvents btnPDF As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents updPDF As OpenFileDialog
+    Friend WithEvents btnSendSubmission As Button
+    Friend WithEvents lblPDFMsg As Label
+    Friend WithEvents lblXMLMsg As Label
+    Friend WithEvents lblManifestMsg As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents txtSubmissionIdAck As TextBox
+    Friend WithEvents lblSubmissionIdAck As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnGetAck As Button
+End Class
---
+++ IRSSubmission.vb
@@ -0,0 +1,225 @@
+﻿Imports MeF.Client
+Imports MeF.Client.Services.MSIServices
+Imports MeF.Client.Services.TransmitterServices
+Imports MeF.Client.Services.InputComposition
+Imports MeF.Client.EfileAttachments
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Web
+Imports RestSharp
+Imports Newtonsoft.Json
+Imports System.Xml
+
+Public Class IRSSubmission
+
+    '********************THIS SECTION FOR UPLOADING MANIFEST FILE ********************************
+    Private Sub btnManifest_Click(sender As Object, e As EventArgs) Handles btnManifest.Click
+
+        'To where your opendialog box get starting location. My initial directory location Is desktop.
+        updManifest.InitialDirectory = "C://Desktop"
+        '//Your opendialog box title name.
+        updManifest.Title = "Select file to be upload."
+        '//which type file format you want to upload in database. just add them.
+        updManifest.Filter = "Select Valid Document(*.xml)| *.xml"
+        '//FilterIndex property represents the index of the filter currently selected in the file dialog box.
+        updManifest.FilterIndex = 1
+        Try
+
+            If updManifest.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                'Cheking for file uploaded by user
+                If (updManifest.CheckFileExists) Then
+                    'Getting the path for the file uploaded by user
+                    Dim Path = System.IO.Path.GetFullPath(updManifest.FileName)
+                    'I used msg box to pop-up the path values to user
+                    MessageBox.Show(Path)
+                    'I also added lbl to application for user, you can do it as per your application need
+                    lblManifestMsg.Text = updManifest.FileName
+                Else
+                    'Here is validations msg
+                    MessageBox.Show("Please Upload document.")
+                End If
+            End If
+
+        Catch ex As Exception
+            'Here goes the error msg if any
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+    End Sub
+
+    '********************THIS SECTION FOR UPLOADING XML FILE ********************************
+    Private Sub btnUploadXML_Click(sender As Object, e As EventArgs) Handles btnUploadXML.Click
+        'To where your opendialog box get starting location. My initial directory location Is desktop.
+        updXML.InitialDirectory = "C://Desktop"
+        '//Your opendialog box title name.
+        updXML.Title = "Select file to be upload."
+        '//which type file format you want to upload in database. just add them.
+        updXML.Filter = "Select Valid Document(*.xml)| *.xml"
+        '//FilterIndex property represents the index of the filter currently selected in the file dialog box.
+        updXML.FilterIndex = 1
+        Try
+            If updXML.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                If (updXML.CheckFileExists) Then
+                    'Get full path for XML file here
+                    Dim Path = System.IO.Path.GetFullPath(updXML.FileName)
+                    'Display to user or handle as per your application structure 
+                    MessageBox.Show(Path)
+                    lblXMLMsg.Text = updXML.FileName
+                Else
+                    'Validation msg goes here
+                    MessageBox.Show("Please Upload document.")
+                End If
+            End If
+        Catch ex As Exception
+            'Here goes the exceptions
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+
+    End Sub
+
+
+    '********************THIS SECTION FOR UPLOADING PDF FILE ********************************
+    Private Sub btnPDF_Click(sender As Object, e As EventArgs) Handles btnPDF.Click
+        'To where your opendialog box get starting location. My initial directory location Is desktop.
+        updPDF.InitialDirectory = "C://Desktop"
+        '//Your opendialog box title name.
+        updPDF.Title = "Select file to be upload."
+        '//which type file format you want to upload in database. just add them.
+        updPDF.Filter = "Select Valid Document(*.pdf)| *.pdf"
+        '//FilterIndex property represents the index of the filter currently selected in the file dialog box.
+        updPDF.FilterIndex = 1
+        Try
+            If updPDF.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                If (updPDF.CheckFileExists) Then
+                    Dim Path = System.IO.Path.GetFullPath(updPDF.FileName)
+                    MessageBox.Show(Path)
+                    lblPDFMsg.Text = updPDF.FileName
+                Else
+                    MessageBox.Show("Please Upload document.")
+                End If
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+    End Sub
+
+    '******************** THIS SECTION FOR SEND SUBMISSIONS ********************************
+    Private Sub btnSendSubmission_Click(sender As Object, e As EventArgs) Handles btnSendSubmission.Click
+
+        Try
+
+            'Create object of SubmissionBuilder over here
+            Dim builder = New SubmissionBuilder()
+            'We need input SubmissionID as input for this
+            Dim SubmissionID As String = txtSubmissionID.Text
+            'Use the manifest file name uploaded earlier and create "manifest" for submission
+            Dim fileBytes = File.ReadAllBytes(updManifest.FileName)
+            Dim manifest64 As String = Convert.ToBase64String(fileBytes)
+            Dim xml641 = File.ReadAllBytes(updXML.FileName)
+            Dim xml64 As String = Convert.ToBase64String(xml641)
+            Dim pdf2 = "" 'File.ReadAllBytes(updPDF.FileName)
+            Dim pdf264 As String = "" 'Convert.ToBase64String(pdf2)
+
+            Dim client = New RestClient("http://aimcorp-002-site6.gtempurl.com/api/values/login")
+            Dim request = New RestRequest(Method.POST)
+            request.AddHeader("cache-control", "no-cache")
+            request.AddHeader("content-type", "application/json")
+            'BELOW IS FOR TESTING
+
+            request.AddParameter("application/json", "{" & vbLf & "    ""etin"": ""24634"", " & vbLf & "    ""appSysId"": ""66011601"", " & vbLf & "    ""submissionId"": """ + SubmissionID + """, " & vbLf & "    ""Manifest"": """ + manifest64 + """, " & vbLf & "    ""XML"": """ + xml64 + """, " & vbLf & "    ""PDF"": """ + pdf264 + """," & vbLf & "    ""environment"": ""P""" & vbLf & "}", ParameterType.RequestBody)
+
+
+            Dim response As IRestResponse = client.Execute(request)
+            Dim checkresult = response
+            Dim item As SubmissionResponse = JsonConvert.DeserializeObject(Of SubmissionResponse)(checkresult.Content)
+            Dim result As String = ""
+            result += "Submission ID : " + item.data.MessageID + Environment.NewLine
+            result += "SubmissionReceivedTs : " + item.data.Relatesto + Environment.NewLine
+            result += "Status : " + item.data.Statustxt + Environment.NewLine
+            MessageBox.Show(result)
+        Catch ex As Exception
+            'Here goes the errors if any
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+
+    End Sub
+
+    '******************** THIS SECTION FOR ACKKNOWLEDGEMENT ********************************
+
+    Private Sub btnGetAck_Click(sender As Object, e As EventArgs) Handles btnGetAck.Click
+        Try
+
+            Dim client = New RestClient("http://aimcorp-002-site6.gtempurl.com/api/values/ack")
+            Dim request = New RestRequest(Method.POST)
+            request.AddHeader("postman-token", "ff39a466-c98d-18ec-2a80-4678ff97bbc0")
+            request.AddHeader("cache-control", "no-cache")
+            request.AddHeader("content-type", "application/json")
+            'BELOW IS FOR TESTING
+
+
+            request.AddParameter("application/json", "{" & vbCrLf & "    ""etin"":""24634""," & vbCrLf & "    ""appSysId"":""66011601""," & vbCrLf & "    ""submissionId"":""" + txtSubmissionIdAck.Text + """," & vbCrLf & "    ""environment"":""P""" & vbCrLf & "}" & vbCrLf, ParameterType.RequestBody)
+
+            Dim response As IRestResponse = client.Execute(request)
+            Dim result = response
+            Dim item As AckResponse = JsonConvert.DeserializeObject(Of AckResponse)(result.Content)
+            Dim root As String = "C://Acknowledgement"
+            If Not Directory.Exists(root) Then
+                Directory.CreateDirectory(root)
+            End If
+            Dim AcceptanceStatusTxt = ""
+            Dim AcceptanceFlag = True
+            Dim ErrorMessageTxt = ""
+            Dim ErrorMessageFlag = True
+            If item.data.content IsNot Nothing AndAlso item.data.content.Length > 0 Then
+                Dim utfString = Encoding.UTF8.GetString(item.data.content, 0, item.data.content.Length)
+                Dim strArrayEle As String() = utfString.Split("<".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+
+                For Each data In strArrayEle
+                    If data.Contains("AcceptanceStatusTxt>") And AcceptanceFlag = True Then
+                        AcceptanceStatusTxt = data.Replace("AcceptanceStatusTxt>", "")
+                        AcceptanceFlag = False
+                        'AcceptanceStatusTxt = AcceptanceStatusTxt.Replace("</AcceptanceStatusTxt>", "")
+                    End If
+                    If data.Contains("ErrorMessageTxt>") And ErrorMessageFlag = True Then
+                        ErrorMessageTxt = data.Replace("ErrorMessageTxt>", "")
+                        ErrorMessageFlag = False
+                        'ErrorMessageTxt = ErrorMessageTxt.Replace("</ErrorMessageTxt>", "")
+                    End If
+                Next
+                File.WriteAllBytes("C://Acknowledgement/" + txtSubmissionIdAck.Text + ".xml", item.data.content)
+            Else
+                ErrorMessageTxt = "No ACK Found with SubmissionID"
+            End If
+
+
+
+            Dim output = "MessageID  :  " + item.data.MessageID + Environment.NewLine + "AcceptanceStatusTxt  :  " + AcceptanceStatusTxt + Environment.NewLine + "ErrorMessageTxt  :  " + ErrorMessageTxt
+            MessageBox.Show(output)
+
+
+        Catch ex As Exception
+            'Here goes the error msgs if any
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+    End Sub
+    Public Class AckResponse
+        Public Property output As String
+        Public Property data As data
+    End Class
+    Public Class data
+        Public Property MessageID As String
+        Public Property content As Byte()
+        Public Property Relatesto As String
+        Public Property Statustxt As String
+    End Class
+    Public Class SubmissionResponse
+        Public Property output As String
+        Public Property data As Submissiondata
+    End Class
+    Public Class Submissiondata
+        Public Property MessageID As String
+        Public Property Relatesto As String
+        Public Property Statustxt As String
+    End Class
+End Class
